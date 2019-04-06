effe5db2f64e7738a80f18bc9b164208da1e43e0
Doug Smith <dsmith47@nd.edu>
2019-04-06 15:56:39 -0400

    Added actual endpoints for API

--- src/app/employee-interface/employee-interface.component.ts
+++ src/app/employee-interface/employee-interface.component.ts
@@ -9,14 +9,44 @@ import { HrApiService } from '../hr-api.service';
 })
 export class EmployeeInterfaceComponent implements OnInit {
 
+  empName:string = "";
+  empId:string = "";
+  empKey:string = "";
+  supKey:string = "";
+
+  assetId:string = "";
+  pubKey:string ="";
+  date:string ="";
+  project:string ="";
+  minutes_worked:string ="";
+
+  lastRequestStatus:string = "";
+
   constructor(private api: HrApiService) { }
 
   ngOnInit() {
+    this.getEmployeeInformation();
   }
 
   getAllTickets() {
-    console.log("Getting All Tickets");
-    console.log(this.api.getTicketIds());
+    this.api.modifyProjectTime(this.assetId, this.pubKey, this.date,
+                               this.project, this.minutes_worked)
+      .subscribe((data) => {
+        console.log(data);
+        this.lastRequestStatus = "submission Logged!"
+        this.getEmployeeInformation();
+      });
   }
 
+  getEmployeeInformation() {
+    this.api.getEmployee('0xcc3f10Dc50eDBc58Ec01Ea8783E1945EF5b6Dc55')
+    .subscribe(((data) => {
+      let d = JSON.parse(data._body).results[0].payload.inputs;
+      
+     this.empName = d["name"];
+     this.empId = d["assetId"];
+     this.empKey = d["public_key"];
+     this.supKey = d["supervisor"];
+    }));
+  }
 }
--- src/app/hr-api.service.ts
+++ src/app/hr-api.service.ts
@@ -1,12 +1,58 @@
 import { Injectable } from '@angular/core';
+import { Http, RequestOptions, Headers, Response } from "@angular/http";
+import { Observable } from "rxjs/Observable";
 
 @Injectable()
 export class HrApiService {
+  baseUrl: string = "https://api.simbachain.com/v1/HR_Chain_API"; 
+  apiKey: string = "6de93c447d58ef885506901e46ff4b7eed978f7eefe9cc9ab4dd6329ea0a7f41";
 
-  constructor() { }
+  getEmployeesEndpoint = "/employee_create/";
+  modifyProjectTimeEndpoint = "/time_card_modify_time/";
 
-  public getTicketIds(): number[] {
-    return [];
+  constructor(private http: Http) {}
+
+  public getTicketIds(): Observable<any> {
+    console.log(this.baseUrl)
+    let options = new RequestOptions({
+    headers: new Headers({
+      'APIKEY': this.apiKey
+    })
+    });
+    return this.http.get(this.baseUrl + this.getEmployeesEndpoint, options);
+  }
+
+  public getEmployee(pubKey:string): Observable<any> {
+    let options = new RequestOptions({
+      headers: new Headers({
+        'APIKEY': this.apiKey
+      })  
+    });
+
+    return this.http.get(this.baseUrl + this.getEmployeesEndpoint,
+                         options);
+  }
+
+  public modifyProjectTime(assetId: string,
+                           pubKey: string,
+			   date: string,
+			   project: string,
+			   minutes_worked: string
+  ): Observable<any> {
+    console.log(this.baseUrl)
+    let options = new RequestOptions({
+      headers: new Headers({
+        'APIKEY': this.apiKey
+      }),
+    });
+    let body:any = {
+      'assetId': assetId,
+      'from': pubKey,
+      'date': date,
+      'project': project,
+      'minutes_worked': minutes_worked
+    };
+    return this.http.post(this.baseUrl + this.modifyProjectTimeEndpoint, body, options);
   }
 
 }
