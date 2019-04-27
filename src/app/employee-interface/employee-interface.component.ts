484995f484d73af5c83e3b5568e31c29a2256005
JamieBodeau <jbodeau@nd.edu>
2019-04-27 13:31:17 -0400

    Revert "my ide automatically changed some formatting, i know it probably doesn't make a huge difference but it means my ide won't get mad at me"
    This reverts commit c44626eb14b4f8e5d642ee8ba8fd18d8cefc3730.

--- src/app/employee-interface/employee-interface.component.ts
+++ src/app/employee-interface/employee-interface.component.ts
@@ -9,18 +9,18 @@ import { HrApiService } from '../hr-api.service';
 })
 export class EmployeeInterfaceComponent implements OnInit {
 
-  empName = '';
-  empId = '';
-  empKey = '';
-  supKey = '';
+  empName:string = "";
+  empId:string = "";
+  empKey:string = "";
+  supKey:string = "";
 
-  assetId = '';
-  pubKey = '';
-  date = '';
-  project = '';
-  minutes_worked = '';
+  assetId:string = "";
+  pubKey:string ="";
+  date:string ="";
+  project:string ="";
+  minutes_worked:string ="";
 
-  lastRequestStatus = '';
+  lastRequestStatus:string = "";
 
   constructor(private api: HrApiService) { }
 
@@ -33,7 +33,7 @@ export class EmployeeInterfaceComponent implements OnInit {
                                this.project, this.minutes_worked)
       .subscribe((data) => {
         console.log(data);
-        this.lastRequestStatus = 'submission Logged!';
+        this.lastRequestStatus = "submission Logged!"
         this.getEmployeeInformation();
       });
   }
@@ -41,12 +41,12 @@ export class EmployeeInterfaceComponent implements OnInit {
   getEmployeeInformation() {
     this.api.getEmployee('0xcc3f10Dc50eDBc58Ec01Ea8783E1945EF5b6Dc55')
     .subscribe(((data) => {
-      const d = JSON.parse(data._body).results[0].payload.inputs;
-
-     this.empName = d['name'];
-     this.empId = d['assetId'];
-     this.empKey = d['public_key'];
-     this.supKey = d['supervisor'];
+      let d = JSON.parse(data._body).results[0].payload.inputs;
+      
+     this.empName = d["name"];
+     this.empId = d["assetId"];
+     this.empKey = d["public_key"];
+     this.supKey = d["supervisor"];
     }));
   }
 }
--- src/app/hr-api.service.ts
+++ src/app/hr-api.service.ts
@@ -1,20 +1,20 @@
 import { Injectable } from '@angular/core';
-import { Http, RequestOptions, Headers, Response } from '@angular/http';
-import { Observable } from 'rxjs/Observable';
+import { Http, RequestOptions, Headers, Response } from "@angular/http";
+import { Observable } from "rxjs/Observable";
 
 @Injectable()
 export class HrApiService {
-  baseUrl = 'https://api.simbachain.com/v1/HR_Chain_API';
-  apiKey = '6de93c447d58ef885506901e46ff4b7eed978f7eefe9cc9ab4dd6329ea0a7f41';
+  baseUrl: string = "https://api.simbachain.com/v1/HR_Chain_API"; 
+  apiKey: string = "6de93c447d58ef885506901e46ff4b7eed978f7eefe9cc9ab4dd6329ea0a7f41";
 
-  getEmployeesEndpoint = '/employee_create/';
-  modifyProjectTimeEndpoint = '/time_card_modify_time/';
+  getEmployeesEndpoint = "/employee_create/";
+  modifyProjectTimeEndpoint = "/time_card_modify_time/";
 
   constructor(private http: Http) {}
 
   public getTicketIds(): Observable<any> {
-    console.log(this.baseUrl);
-    const options = new RequestOptions({
+    console.log(this.baseUrl)
+    let options = new RequestOptions({
     headers: new Headers({
       'APIKEY': this.apiKey
     })
@@ -22,11 +22,11 @@ export class HrApiService {
     return this.http.get(this.baseUrl + this.getEmployeesEndpoint, options);
   }
 
-  public getEmployee(pubKey: string): Observable<any> {
-    const options = new RequestOptions({
+  public getEmployee(pubKey:string): Observable<any> {
+    let options = new RequestOptions({
       headers: new Headers({
         'APIKEY': this.apiKey
-      })
+      })  
     });
 
     return this.http.get(this.baseUrl + this.getEmployeesEndpoint,
@@ -39,13 +39,13 @@ export class HrApiService {
 			   project: string,
 			   minutes_worked: string
   ): Observable<any> {
-    console.log(this.baseUrl);
-    const options = new RequestOptions({
+    console.log(this.baseUrl)
+    let options = new RequestOptions({
       headers: new Headers({
         'APIKEY': this.apiKey
       }),
     });
-    const body: any = {
+    let body:any = {
       'assetId': assetId,
       'from': pubKey,
       'date': date,
