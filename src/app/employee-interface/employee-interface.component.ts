c44626eb14b4f8e5d642ee8ba8fd18d8cefc3730
JamieBodeau <jbodeau@nd.edu>
2019-04-27 12:56:18 -0400

    my ide automatically changed some formatting, i know it probably doesn't make a huge difference but it means my ide won't get mad at me

--- src/app/employee-interface/employee-interface.component.ts
+++ src/app/employee-interface/employee-interface.component.ts
@@ -9,18 +9,18 @@ import { HrApiService } from '../hr-api.service';
 })
 export class EmployeeInterfaceComponent implements OnInit {
 
-  empName:string = "";
-  empId:string = "";
-  empKey:string = "";
-  supKey:string = "";
+  empName = '';
+  empId = '';
+  empKey = '';
+  supKey = '';
 
-  assetId:string = "";
-  pubKey:string ="";
-  date:string ="";
-  project:string ="";
-  minutes_worked:string ="";
+  assetId = '';
+  pubKey = '';
+  date = '';
+  project = '';
+  minutes_worked = '';
 
-  lastRequestStatus:string = "";
+  lastRequestStatus = '';
 
   constructor(private api: HrApiService) { }
 
@@ -33,7 +33,7 @@ export class EmployeeInterfaceComponent implements OnInit {
                                this.project, this.minutes_worked)
       .subscribe((data) => {
         console.log(data);
-        this.lastRequestStatus = "submission Logged!"
+        this.lastRequestStatus = 'submission Logged!';
         this.getEmployeeInformation();
       });
   }
@@ -41,12 +41,12 @@ export class EmployeeInterfaceComponent implements OnInit {
   getEmployeeInformation() {
     this.api.getEmployee('0xcc3f10Dc50eDBc58Ec01Ea8783E1945EF5b6Dc55')
     .subscribe(((data) => {
-      let d = JSON.parse(data._body).results[0].payload.inputs;
-      
-     this.empName = d["name"];
-     this.empId = d["assetId"];
-     this.empKey = d["public_key"];
-     this.supKey = d["supervisor"];
+      const d = JSON.parse(data._body).results[0].payload.inputs;
+
+     this.empName = d['name'];
+     this.empId = d['assetId'];
+     this.empKey = d['public_key'];
+     this.supKey = d['supervisor'];
     }));
   }
 }
