76007f6066b52d917fbde2097e098f89f8e2b25b
JamieBodeau <jbodeau@nd.edu>
2019-05-02 15:39:37 -0400

    Closer to what simba wants

--- src/app/metamask.service.ts
+++ src/app/metamask.service.ts
@@ -36,25 +36,36 @@ export class MetamaskService {
   public sign(message: string) {
     const web3 = window['web3'];
 
+    const payload: string = JSON.stringify(JSON.parse(message).payload)
+
     web3.currentProvider.sendAsync({ id: 1, method: 'personal_sign', params: [web3.eth.accounts[0], message] },
-    this.generateWeb3Callback(message, JSON.parse(message)['id'],
-    this.http, 'https://api.simbachain.com/v1/HR_Chain_V4/transactions',
+    this.generateWeb3Callback(message,
+    this.http, 'https://api.simbachain.com/v1/HR_Chain_V4/transaction',
     '497e05cc72e9b0121cce9363f06721ebd79ffbe9872292208bcb8e240bb48d92'));
 
   }
 
 
-  public generateWeb3Callback(message_text, id_str,
-                              http, url, apikey) {
+  public generateWeb3Callback(message, http, url, apikey) {
+    const messageDict = JSON.parse(message)
+    const id_str = messageDict.id
+
+    const to = messageDict.payload.raw.to
+    const data = messageDict.payload.raw.data.substring(2)
+
     return function(err, result) {
       const sig = result.result;
+
+      // FIXME: We were unable to figure out exactly which data is supposed to be here and which data
+      // FIXME: we were supposed to sign. Their documentation doesn't specify it anywhere.
+      const payload = to + data + sig.substring(2)
+      console.log(payload)
       const body = {
-        'body': message_text,
-        'txn_id': id_str,
+        'payload': payload,
       };
       const options = new RequestOptions({
         headers: new Headers({
-          'APIKEY': apikey,
+          'APIKEY': apikey
           }),
       });
       http.post(url + '/' + id_str + '/', body, options).subscribe((data) => {console.log(data); });
